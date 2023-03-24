67bd2026f29e3f132c4ded55f602b0d2cf7bb41e
Muhammad Raihan Iqbal <raihanvega24@gmail.com>
2023-03-24 23:43:46 +0700

    add report file

--- src/Tubes2_mth/Tubes2_mth/obj/Release/net6.0-windows/MainWindow.g.cs
+++ src/Tubes2_mth/Tubes2_mth/obj/Release/net6.0-windows/MainWindow.g.cs
@@ -77,7 +77,7 @@ namespace Tubes2_mth {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Tubes2_mth;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Tubes2_mth;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
